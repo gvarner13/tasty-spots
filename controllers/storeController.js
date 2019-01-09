cce7c84bdc06534b32788cc85f118abd3f67f5fe
Gary Varner <garysarahvarner@gmail.com>
2019-01-09 08:04:41 -0500

    Chaned the update to include location type as point for lat lng

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -26,6 +26,8 @@ exports.editStore = async (req, res) => {
 };
 
 exports.updateStore = async (req, res) => {
+    // set the location to be a point
+    req.body.location.type = 'Point';
     const store = await Store.findOneAndUpdate({_id: req.params.id}, req.body, {
         new: true, 
         runValidators: true
