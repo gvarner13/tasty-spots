db26904b6bf11dfd24144c882e9fb283a51ca013
Gary Varner <garysarahvarner@gmail.com>
2019-01-10 14:16:09 -0500

    added base single store view

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -38,7 +38,7 @@ exports.resize = async (req, res, next) => {
     await photo.resize(800, jimp.AUTO);
     await photo.write(`./public/uploads/${req.body.photo}`);
     // once resize is done continue to store
-    next(); 
+    next();
 };
 
 exports.createStore = async (req, res) => {
@@ -68,3 +68,9 @@ exports.updateStore = async (req, res) => {
     req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store</a>`);
     res.redirect(`/stores/${store._id}/edit`);
 };
+
+exports.getStoreBySlug = async (req, res, next) => {
+    const store = await Store.findOne({slug: req.params.slug});
+    if(!store) return next();
+    res.render('store', {store, title: store.name});
+}
