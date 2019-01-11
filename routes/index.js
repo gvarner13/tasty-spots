418b5bd791ea1c5e67f7de0914b42590d06a81fb
Gary Varner <garysarahvarner@gmail.com>
2019-01-11 08:40:41 -0500

    Removinf odd whitespace from fat finger

--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,12 @@ router.get('/', catchErrors(storeController.getStores));
 router.get('/stores', catchErrors(storeController.getStores));
 router.get('/add', storeController.addStore);
 router.post('/add', 
-    storeController.upload, 
-    catchErrors(storeController.resize), 
+    storeController.upload,
+    catchErrors(storeController.resize),
     catchErrors(storeController.createStore)
 );
 router.post('/add/:id',
-    storeController.upload, 
+    storeController.upload,
     catchErrors(storeController.resize),
     catchErrors(storeController.updateStore)
 );
