82fc5a8d45e7baa93b68f31507e4e9e1a8cea629
Gary Varner <garysarahvarner@gmail.com>
2019-01-15 22:00:30 -0500

    Changed flash message with correct store link

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -65,7 +65,7 @@ exports.updateStore = async (req, res) => {
         new: true, 
         runValidators: true
     }).exec();
-    req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store</a>`);
+    req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/store/${store.slug}">View Store</a>`);
     res.redirect(`/stores/${store._id}/edit`);
 };
 
